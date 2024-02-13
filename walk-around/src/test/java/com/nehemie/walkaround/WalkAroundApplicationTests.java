7408efd97b75d98c326a35c5f06868df109e8c95
NehemieMbg <nehemie.mbg@gmail.com>
2024-02-13 15:04:23 +0100

    Working with Beans

---
+++ walk-around/src/test/java/com/nehemie/walkaround/WalkAroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nehemie.walkaround;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkAroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
